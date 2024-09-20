a62235480068373e1a5dd98adac0a702b0c6a0d0
github-anuk <160622798+github-anuk@users.noreply.github.com>
2024-09-20 14:04:57 +0200

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,52 @@
+class Tree:
+    def __init__(self,x):
+        self.data=x
+        self.left=None
+        self.right=None
+def inorder(root):
+    if root:
+        if root.left is not None:
+            inorder(root.left)
+        print(root.data)
+        if root.right is not None:
+            inorder(root.right)
+
+def insert(root,l):
+    if root is None:
+        return Tree(l)
+    elif root.data >l:
+        root.left = insert(root.left,l)
+    else:
+        root.right=insert(root.right,l)
+    return root
+
+def serch(root,m):
+    if root.data==m:
+        return root
+    elif root.data >m and root.left is not None:
+        return serch(root.left,m)
+    elif root.data < m and root.right is not None:
+        return serch(root.right,m)
+    else:
+        return -1
+ATE=int(input("option 1 -- input, option 2 -- serch  "))
+
+if ATE ==1 :
+    ml=int(input("ENTER HOW MUCH FOOD HAVE YOU GOT FOR TREE: "))
+    root=None
+    for i in range(ml):
+        t=int(input("WHAT FOOD IS THAT YOU HAVE: "))
+        root=insert(root,t)
+    inorder(root)
+else:
+    ml=int(input("ENTER HOW MUCH FOOD HAVE YOU GOT FOR TREE: "))
+    root=None
+    for i in range(ml):
+        t=int(input("WHAT FOOD IS THAT YOU HAVE: "))
+        root=insert(root,t)
+    s=int(input("ENTER WHICH FOOD YOU WANT TO SEE IN TREES STOMACH: "))
+    se=serch(root,s)
+    if se == -1:
+        print("NO SUCH FOOD IS FOUND IN TREE'S STOMACH ")
+    else:
+        print("yes, TREE ATE ",se.data)
